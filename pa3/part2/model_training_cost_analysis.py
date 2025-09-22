8656f637e2b3ba717ed5bf47932db3992e463bdc
Nicooo-Wang <70059290+Nicooo-Wang@users.noreply.github.com>
2025-09-22 14:32:53 +0800

    finish llama 7b cost

--- pa3/part2/model_training_cost_analysis.py
+++ pa3/part2/model_training_cost_analysis.py
@@ -3,8 +3,45 @@ import json
 import math
 
 def model_training_cost_analysis_llama(model_config_path):
-    #TODO you code here.
-    
+    with open(model_config_path, "r") as f:
+        data = json.load(f)
+
+    vocab_size = data["vocab_size"]
+    hidden_size = data["hidden_size"]
+    intermediate_size = data["intermediate_size"]
+    num_hidden_layers = data["num_hidden_layers"]
+    num_attention_heads = data["num_attention_heads"]
+    max_sequence_length = data["max_sequence_length"]
+
+    # params
+    params_emb = vocab_size * hidden_size
+    params_layer_norm = hidden_size
+    params_self_atten = 4 * hidden_size * hidden_size
+    params_ffn = 3 * hidden_size * intermediate_size
+    params_linear_trans = vocab_size * hidden_size
+    total_params = (
+        num_hidden_layers * (params_layer_norm * 2 + params_self_atten + params_ffn)
+        + params_emb
+        + params_linear_trans
+    )
+
+    # flops
+    flops_layer_TF = (
+        6 * max_sequence_length * hidden_size**2
+        + 4 * max_sequence_length**2 * hidden_size
+        + 3 * max_sequence_length**2 * num_attention_heads
+        + 2 * max_sequence_length * hidden_size**2
+        + 6 * max_sequence_length * hidden_size * intermediate_size
+    ) / 1e12
+
+    # mem
+    params_per_layer = params_layer_norm * 2 + params_self_atten + params_ffn
+    peak_memory_GB = 0
+    peak_memory_GB += 2 * params_per_layer  # weights
+    peak_memory_GB += 12 * params_per_layer  # weights
+    peak_memory_GB += 2 * hidden_size * max_sequence_length # activations
+    peak_memory_GB /= 1e9
+
     return total_params, flops_layer_TF, peak_memory_GB
 
 def model_training_cost_analysis_deepseek(model_config_path):
